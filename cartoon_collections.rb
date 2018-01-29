f89ed8095623ecd4051400b1cc36c93af69eb3ac
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 18:36:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(array)
   array.map { |item|
      item[0] = item[0].capitalize
      item = item+"!"
-   }   
+   }
 end
 
 def long_planeteer_calls(array)# code an argument here
@@ -18,7 +18,7 @@ def long_planeteer_calls(array)# code an argument here
 end
 
 def find_the_cheese(array)
-  
+
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each {|cheese|
      if array.include?(cheese)
